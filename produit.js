aa3992ccc7982e921b78bb1b5e1880c8faa4eb50
KimoAth <99965559+KimoAth@users.noreply.github.com>
2022-05-01 01:17:37 +0200

    Add files via upload

---
+++ produit.js
@@ -0,0 +1,34 @@
+function more_product(){
+        let product1=document.getElementsByClassName('pic1');
+        if (product1.src.match("milk.jpeg")) {
+            product1.src = "milk.jpeg";
+        }
+        let product2=document.getElementsByClassName('pic2');
+        if (product2.src.match("lben.jpeg")){    
+            product2.src = "zebda.jpeg";
+        }
+        let product3=document.getElementsByClassName('pic3');
+        if (product3.src.match("rayebmess.jpeg")){    
+            product3.src = "rayebmess.jpeg";
+        }
+        let product4=document.getElementsByClassName('pic4');
+        if (product4.src.match("rayeb.jpeg")){    
+            product4.src = "rayeb.jpeg";
+        }
+        let product5=document.getElementsByClassName('pic5');
+        if (product5.src.match("zebda.jpeg")){    
+            product5.src = "zebda.jpeg";
+        }
+        let product6=document.getElementsByClassName('pic6');
+        if (product6.src.match("smen.jpeg")){    
+            product6.src = "smen.jpeg";
+        }
+        let product7=document.getElementsByClassName('pic7');
+        if (product7.src.match("jben.jpeg")){    
+            product7.src = "jben.jpeg";
+        }
+        let product8=document.getElementsByClassName('pic8');
+        if (product8.src.match("jbnzaater.jpeg")){    
+            product8.src = "jbnzaater.jpeg";
+        }
+}
